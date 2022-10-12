0d585e731f335c75100d4c6a8112c0303574bcdb
alamin <amin.se.aiub@gmail.com>
2022-10-12 13:50:47 +0600

    php basic+form data

---
+++ php/test.php
@@ -0,0 +1,55 @@
+<?php
+
+    $id = 1;
+    $name = "alamin";
+    $cgpa = 2.5;
+    $email = "alamin@aiub.edu";
+
+    //$student  = array(1, 'alamin', 2.5, 'alamin@aiub.edu');
+    $student  = [1, 'alamin', 2.5, 'alamin@aiub.edu'];
+    // $students  = [
+    //                 [1, 'alamin', 2.5, 'alamin@aiub.edu'],
+    //                 [2, 'abc', 2.5, 'alamin@aiub.edu'],
+    //                 [3, 'xyz', 2.5, 'alamin@aiub.edu']
+    //             ];
+    // $students[2][3];
+    
+    // $student  = ['id'=>1, 'name'=>'alamin', 'cgpa'=>2.5, 'email'=>'alamin@aiub.edu'];
+    // $student['name'];
+    
+    // $students  =[ 
+    //                 's1'=>['id'=>1, 'name'=>'alamin', 'cgpa'=>2.5, 'email'=>'alamin@aiub.edu'],
+    //                 's2'=>['id'=>2, 'name'=>'xyz', 'cgpa'=>2.6, 'email'=>'xyz@aiub.edu'],
+    //                 's3'=>['id'=>3, 'name'=>'abc', 'cgpa'=>2.7, 'email'=>'abc@aiub.edu']
+    //             ];
+
+    // $students['s2']['email'];
+
+    function sum($a=0, $b=0){
+        return $a+$b;
+    }
+
+    //echo sum(5);
+
+    // if("10" === 10){
+    //     echo "true";
+    // }else{
+    //     echo "false";
+    // }
+
+    for($i=0; $i<10; $i++){
+        echo "<h3>Alamin<h3>";
+    }
+
+    //echo $student[0]." ".$student[2];
+    //print($id);
+    //print_r($student);
+
+    //processing
+
+    // per store
+    //output
+
+?>
+
+<h5>test</h5>
